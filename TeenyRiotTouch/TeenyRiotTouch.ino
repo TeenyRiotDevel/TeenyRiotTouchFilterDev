610e020dac55d4d6468072704a07522e8b07d403
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2016-04-13 14:23:11 +0200

    works with 2 channel, without main power sometimes crash

--- TeenyRiotTouch/TeenyRiotTouch.ino
+++ TeenyRiotTouch/TeenyRiotTouch.ino
@@ -21,61 +21,20 @@ MIDIMessage midimsg;
 // * indicates PWM port
 //
 
-int i = -1023;
-int value = 0;
-int value2 = 0;
-int prevValue = 0;
-int velocityValue = 0;
+int value[8] = {0,0,0,0,0,0,0,0};
+//int prevValue = 0;
+//int velocityValue = 0;
+uint16_t offset_adc[8] = {0,0,0,0,0,0,0,0};
+SampleFilter filter_samp[8];
 
-int prevValue2 = 0;
-int velocityValue2 = 0;
 
-uint8_t samples;
-uint8_t midi_delay;
-uint8_t sense_mode;
-uint8_t send_mode;
-uint8_t multiplex_ch;
-uint8_t multiplex_ch2;
-
-uint16_t timer = 0;
-uint8_t note_off = 0;
-uint8_t multiplexer_mapping[8]  = {5,7,6,4,3,0,1,2}; //remap multiplexer pin
+uint8_t multiplexer_mapping[8]  = {6,7,6,4,3,0,1,2}; //remap multiplexer pin
 
 unsigned long previousMillis = 0;        // will store last time LED was updated
-const uint8_t interval = 16;           // interval at which to blink (milliseconds)
-
-uint16_t offset_adc = 0;
-
-SampleFilter filter_samp;
-
-//class filter
-//{
-//    public:
-//        filter()
-//        {
-//            for(int i=0; i <= 2; i++)
-//                v[i]=0;
-//        }
-//    private:
-//        short v[3];
-//    public:
-//        short step(short x)
-//        {
-//            v[0] = v[1];
-//            v[1] = v[2];
-//            long tmp = ((((x * 1372318L) >>  3)	//= (   1.6359307451e-1 * x)
-//                + ((v[0] * -1937502L) >> 3)	//+( -0.2309681871*v[0])
-//                + ((v[1] * 1209209L) >> 1)	//+(  0.5765958891*v[1])
-//                )+524288) >> 20; // round and downshift fixed point /1048576
-
-//            v[2]= (short)tmp;
-//            return (short)((
-//                 (v[0] + v[2])
-//                +2 * v[1])); // 2^
-//        }
-//};
 
 
+#define PIN_SELECT 0
+#define NUM_CHANNEL 2
 
 void setAnalogMultiplexCh(const uint8_t _pin_index)
 {
@@ -91,127 +50,38 @@ void setAnalogMultiplexCh(const uint8_t _pin_index)
 
 }
 
-
-void delay(uint8_t * us)
-{
-    TeenyMidi.delay_us(*us);
-}
-
 void usb_poll()
 {
     usbPoll();
 }
 
-void wait_for_tick(void)
-{
-    unsigned long timestamp;
-    timestamp = millis();
-    while(millis() == timestamp)
-        ;
-}
-
-
-void wait_for_five_ticks(void)
-{
-    wait_for_tick();
-    wait_for_tick();
-    wait_for_tick();
-    wait_for_tick();
-    wait_for_tick();
-}
-
-
-uint8_t readCapacitivePin(uint8_t adcPin) {
-  // Variables used to translate from Arduino to AVR pin naming
-//  volatile uint8_t* port;
-//  volatile uint8_t* ddr;
-//  volatile uint8_t* pin;
-  // Here we translate the input pin number from
-  //  Arduino pin number to the AVR PORT, PIN, DDR,
-  //  and which bit of those registers we care about.
-//  byte bitmask;
-//  port = portOutputRegister(digitalPinToPort(pinToMeasure));
-//  ddr = portModeRegister(digitalPinToPort(pinToMeasure));
-//  bitmask = digitalPinToBitMask(pinToMeasure);
-//  pin = portInputRegister(digitalPinToPort(pinToMeasure));
-  // Discharge the pin first by setting it low and output
-//  *port &= ~(bitmask);
-//  *ddr  |= bitmask;
-
-  PORTB &= ~(1<<adcPin);
-  DDRB |= (1<<adcPin);
-
-
-  //delayMicroseconds(100);
-  delay(1);
-  // Make the pin an input with the internal pull-up on
-//  *ddr &= ~(bitmask);
-//  *port |= bitmask;
-
-  DDRB &= ~(1<<adcPin);
-  PORTB |= (1<<adcPin);
-
-  // Now see how long the pin to get pulled up. This manual unrolling of the loop
-  // decreases the number of hardware cycles between each read of the pin,
-  // thus increasing sensitivity.
-  uint8_t cycles = 17;
-       if (PINB & (1<<adcPin)) { cycles =  0;}
-  else if (PINB & (1<<adcPin)) { cycles =  1;}
-  else if (PINB & (1<<adcPin)) { cycles =  2;}
-  else if (PINB & (1<<adcPin)) { cycles =  3;}
-  else if (PINB & (1<<adcPin)) { cycles =  4;}
-  else if (PINB & (1<<adcPin)) { cycles =  5;}
-  else if (PINB & (1<<adcPin)) { cycles =  6;}
-  else if (PINB & (1<<adcPin)) { cycles =  7;}
-  else if (PINB & (1<<adcPin)) { cycles =  8;}
-  else if (PINB & (1<<adcPin)) { cycles =  9;}
-  else if (PINB & (1<<adcPin)) { cycles = 10;}
-  else if (PINB & (1<<adcPin)) { cycles = 11;}
-  else if (PINB & (1<<adcPin)) { cycles = 12;}
-  else if (PINB & (1<<adcPin)) { cycles = 13;}
-  else if (PINB & (1<<adcPin)) { cycles = 14;}
-  else if (PINB & (1<<adcPin)) { cycles = 15;}
-  else if (PINB & (1<<adcPin)) { cycles = 16;}
-
-  // Discharge the pin again by setting it low and output
-  //  It's important to leave the pins low if you want to
-  //  be able to touch more than 1 sensor at a time - if
-  //  the sensor is left pulled high, when you touch
-  //  two sensors, your body will transfer the charge between
-  //  sensors.
-//  *port &= ~(bitmask);
-//  *ddr  |= bitmask;
-
-       PORTB &= ~(1<<adcPin);
-       DDRB |= (1<<adcPin);
-
-  return cycles;
-}
-// End of capacitive sensing code
-
-
 void setup()
 {
-    SampleFilter_init(&filter_samp);
+
 
     TeenyMidi.init();
 
     TeenyTouchDusjagr.begin();
     TeenyTouchDusjagr.setAdcSpeed(5);
     TeenyTouchDusjagr.delay = 8;
-    TeenyTouchDusjagr.delay_cb = &delay;
+    //TeenyTouchDusjagr.delay_cb = &delay;
     TeenyTouchDusjagr.usb_poll = &usb_poll;
 
-    midi_delay = 5;
-    sense_mode = 1;
-    send_mode = 0;
-    samples = 100;
-    multiplex_ch = 0;
-    multiplex_ch = 0;
+//    midi_delay = 5;
+//    sense_mode = 1;
+//    send_mode = 0;
+//    samples = 100;
+//    multiplex_ch = 0;
+//    multiplex_ch = 0;
 
-    pinMode(PB0, OUTPUT);
-    setAnalogMultiplexCh(2);
-    offset_adc = TeenyTouchDusjagr.sense(PB4,PB3, 8 );
+//    pinMode(PB0, OUTPUT);
+
+    for (uint8_t i = 0; i < NUM_CHANNEL; i++)
+    {
+        setAnalogMultiplexCh(i);
+        offset_adc[i] = TeenyTouchDusjagr.sense(PB4,PB3, 8 );
+    }
+
 
     TeenyMidi.delay(500);
 }
@@ -222,15 +92,24 @@ void loop()
 
     if (millis()-previousMillis >= 16)  {  // 0% data loss
         //TeenyMidi.sendCCHires(value, 1);
-        TeenyMidi.sendCCHires(SampleFilter_get(&filter_samp), 1);
+        for (uint8_t i = 0; i < NUM_CHANNEL; i++)
+        {
+            TeenyMidi.sendCCHires(SampleFilter_get(&filter_samp[i]), (4*i)+1);
+        }
         previousMillis = millis();
     }
 
   //sample 8 times -> good -> sometimes usb disconnect
   // sample 6 times -> good -> still testing
-  value = TeenyTouchDusjagr.sense(PB4,PB3, 6 ) -offset_adc;
+  for (uint8_t i = 0; i < NUM_CHANNEL; i++)
+  {
+      setAnalogMultiplexCh(i);
+      value[i] = TeenyTouchDusjagr.sense(PB4,PB3, 6 ) - offset_adc[i];
+      if (value[i] > 0) SampleFilter_put(&filter_samp[i], value[i]);
+      TeenyMidi.update();
+  }
 
-  if (value > 0) SampleFilter_put(&filter_samp, value);
+
 
   TeenyMidi.delay(4);
 
